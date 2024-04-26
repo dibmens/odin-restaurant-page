3c9971add6b4d3a7dfde4003b6671924755a770d
dibmens <dibmens@gmail.com>
2024-04-27 00:42:07 +0300

    Add and style logo and navigation menu

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ a simple menu generation practice
 ACKNOWLEDGMENTS
 
 Fonts by Google @ https://fonts.google.com/
+Generated Images by Bing Copilot @ https://www.bing.com/chat
+Image of Patrice O'Neal by Photofest @ https://photofestnyc.com/
+Background Image by Rudy and Peter Skitterians @ https://skitterphoto.com/
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css';
-import Icon from './dibmens.png';
 
 console.log('this is a test');
 
-document.querySelector(`body`).style.backgroundColor = "firebrick";
 document.querySelector(`h1`).classList.add(`hello`);
 
 let buttons = document.querySelectorAll('button');
@@ -12,7 +10,12 @@ buttons.forEach(button => button.addEventListener('click', () => {
     alert('testo');
 }));
 
-const myIcon = new Image();
-myIcon.src = Icon;
+// const myIcon = new Image();
+// myIcon.src = Icon;
 
-document.querySelector(`div`).appendChild(myIcon);
+// document.querySelector(`div`).appendChild(myIcon);
+
+// MENU
+// Cheetah Heels
+// Blue Whale Blowhole
+// 
