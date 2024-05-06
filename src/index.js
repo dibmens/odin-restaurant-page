2a1946f98105df18f34f33f9b3f6bbc7c02bf6d8
dibmens <dibmens@gmail.com>
2024-05-06 23:33:42 +0300

    Add dynamic content for the Menu page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
 import './style.css';
+import Menu from './assets/data/menu.json';
+
 import Img1 from './assets/images/backgrounds/1.jpg';
 import Img2 from './assets/images/backgrounds/2.jpg';
 import Img3 from './assets/images/backgrounds/3.jpg';
 import Img4 from './assets/images/backgrounds/4.jpg';
-import Img5 from './assets/images/backgrounds/5.jpg';
-import Img6 from './assets/images/backgrounds/6.jpg';
-import Img7 from './assets/images/backgrounds/7.jpg';
 
-console.log('this is a test');
+
+const content = document.querySelector(`#content`);
 
 document.querySelector(`h1`).classList.add(`hello`);
 
 let buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    // randomizeBackground();
+    button.classList.contains("menu") ? showMenu() : content.innerText = "";
 }));
 
-function Menu(){
-    
-    
-};
 
 
 function randomizeBackground(){
-    let body = document.querySelector(`body`);
-    let backgrounds = [Img1, Img2, Img3, Img4, Img5, Img6, Img7];
-    let randomize = () => {
+    const body = document.querySelector(`body`);
+    const backgrounds = [Img1, Img2, Img3, Img4];
+    const randomize = () => {
         return Math.trunc(Math.random()*backgrounds.length);
     } 
-    // let number = randomize()
     body.style.backgroundImage = `url(${backgrounds[randomize()]})`;
 };
 
-setInterval(randomizeBackground,12000);
+setInterval(randomizeBackground,15000);
+
+
+
+
+
+function showMenu(){
+  Menu.forEach((courseEntry)=> {
+  let div = document.createElement(`div`);
+  div.className = "course-wrapper";
+  content.append(div);
+
+  let course = document.createElement(`h2`)
+  course.innerText = `${courseEntry["course"].toUpperCase()}`;
+  div.append(course);
+
+  courseEntry["menuItems"].forEach((itemEntry)=> {
+    let item = document.createElement(`h3`);
+    let info = document.createElement(`p`);
+    item.innerText = `${itemEntry["item"].toUpperCase()}`;
+    info.innerText = `${itemEntry["info"]}`;
+    div.append(item);
+    div.append(info);
+  })
+});
+}
