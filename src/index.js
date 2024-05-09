61a4ed3e7f0cf0d88a192b15fe4d191bf57ac9d7
dibmens <dibmens@gmail.com>
2024-05-09 17:23:47 +0300

    Add Media page content

--- src/index.js
+++ src/index.js
@@ -21,8 +21,9 @@ buttons.forEach(button => button.addEventListener('click', () => {
     } else if (button.classList.contains("home")){
       content.innerText = "";
       showHomePage();
-    } else {
+    } else if(button.classList.contains("media")){
       content.innerText = "";
+      showMediaPage();
     }
 }));
 
@@ -69,14 +70,12 @@ function showHomePage(){
   let div = document.createElement(`div`);
   div.className = "home-wrapper";
   div.innerHTML = 
-  `
-  <h3>THE LAST WORD IN HUMAN EXCESS</h3>
+  `<h3>THE LAST WORD IN HUMAN EXCESS</h3>
   <p>Forget the pursuit of excellence, the golden foils and design cutlery. 
   What we desire can't be found in boutique wine cellars, family butchers' 
   or alpine gardens. The meal is circumstantial. We explore the limits of 
   excess. Seek out the most fragile animals on the planet, steal their most 
-  defining feature- their very essence- and feast on it unapologetically.</p>
-  `;
+  defining feature- their very essence- and feast on it unapologetically.</p>`;
   content.append(div);
 
   Reviews.forEach((reviewEntry) => {
@@ -90,5 +89,27 @@ function showHomePage(){
   });
 };
 
-
-// showHomePage();
+function showMediaPage(){
+  let div = document.createElement(`div`);
+  div.className = "home-wrapper";
+  div.innerHTML = 
+  `<h2>
+    Just to take what the animal needs to survive. <br>
+    That's so f****d up. They need their eyes!
+  </h2>
+  <h3>PATRICE O'NEAL<br>1969 - 2011</h3>
+  <p>A New York native stand-up comedian, Patrice came to prominence for 
+    his hilarious guest roles on various comedy shows throughout the 00s. Armed with a 
+    sharp tongue, he rarely made fast friends, but those who got to know 
+    him well saw a man with a big heart and still miss him. The restaurant 
+    menu is inspired by one such guest appearance.
+  </p>
+  <p>Disclaimer- video contains strong language</p>
+  <iframe width="500" height="280" src="https://www.youtube.com/embed/VikTiogGfLs" 
+  title="Patrice O&#39;Neal on O&amp;A - Eating Exotic Animals" 
+  frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+  encrypted-media; gyroscope; picture-in-picture; web-share" 
+  referrerpolicy="strict-origin-when-cross-origin">
+  </iframe>`;
+  content.append(div);
+};
